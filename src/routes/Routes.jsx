9ddcc038a6131c7f1cdb8afc8089b138cad9f9bc
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-09 20:01:38 +0600

    Route setup completed

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import Home from "../components/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../components/Root/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
