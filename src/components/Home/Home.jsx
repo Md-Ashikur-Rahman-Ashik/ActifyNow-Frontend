386cd69902e1fe687cfae73df02c0276b8c81630
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-10 20:17:11 +0600

    Adding need volunteer section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import NeedVolunteer from "../NeedVolunteer/NeedVolunteer";
 
 const Home = () => {
   return (
     <div className="min-h-[calc(100vh-349px)] mx-2 md:mx-10">
       <Banner></Banner>
+      <NeedVolunteer></NeedVolunteer>
     </div>
   );
 };
---
+++ src/components/NeedVolunteer/NeedVolunteer.jsx
@@ -0,0 +1,9 @@
+const NeedVolunteer = () => {
+  return (
+    <div className="hero container p-6 mx-auto min-h-[calc(100vh-349px)]">
+      <h2>This is Need Volunteer Section</h2>
+    </div>
+  );
+};
+
+export default NeedVolunteer;
