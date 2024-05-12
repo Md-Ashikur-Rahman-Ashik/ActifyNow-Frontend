ffac94dc778cdfb713b612163771f5d3d1e77bdd
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-12 09:51:55 +0600

    Updated managed my post card

--- src/components/ManagePost/ManagePost.jsx
+++ src/components/ManagePost/ManagePost.jsx
@@ -1,11 +1,21 @@
 import { useLoaderData } from "react-router-dom";
+import MyCard from "../MyCard/MyCard";
 
 const ManagePost = () => {
   const volunteer = useLoaderData();
-    console.log(volunteer);
+  // console.log(volunteer);
   return (
     <div className="hero container p-6 mx-auto min-h-[calc(100vh-349px)]">
-      {volunteer.length === 0 && <h2 className="font-bold text-center text-5xl text-blue-400">Your Added Item will be shown here</h2>}
+      {volunteer.length === 0 && (
+        <h2 className="font-bold text-center text-5xl text-blue-400">
+          Your Added Item will be shown here
+        </h2>
+      )}
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {volunteer?.map((vol) => (
+          <MyCard key={vol._id} vol={vol}></MyCard>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/MyCard/MyCard.jsx
+++ src/components/MyCard/MyCard.jsx
@@ -1,11 +1,39 @@
+import { Link } from "react-router-dom";
 
+const MyCard = ({ vol }) => {
+  const { postTitle, thumbnail, categoryBox, date, _id } = vol;
 
-const MyCard = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="card bg-base-100 shadow-xl hover:scale-105 transition-transform">
+      <figure>
+        <img src={thumbnail} className="h-56 w-full" alt="Album" />
+      </figure>
+      <div className="p-8">
+        <div className="flex justify-between mb-2">
+          <p className="font-bold">{categoryBox}</p>
+          <p className="font-bold text-red-500">Deadline: {date}</p>
         </div>
-    );
+        <h2 className="text-xl font-bold text-blue-400 mb-4 text-center">
+          {postTitle}
+        </h2>
+        <div>
+          <Link to={`/volunteer/${_id}`}>
+            <button className="btn mb-3 text-blue-400 bg-blue-50 font-bold w-full">
+              View Details
+            </button>
+          </Link>
+          <Link>
+            <button className="btn mb-3 text-blue-400 bg-blue-50 font-bold w-full">
+              Update Post
+            </button>
+          </Link>
+          <button className="btn text-red-500 bg-blue-50 font-bold w-full">
+            Delete Post
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyCard;
